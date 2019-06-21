450b671100f020b24efcc4151ecb52d0d6d89305
hehao00 <13663474696@163.com>
2019-06-21 15:10:35 +0800

    登陆

---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHome',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>login</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLogin',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style  lang="less" scoped>
+
+</style>
