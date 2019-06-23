ff4f01d66eb82ba043c1576f19582a8497607ebf
hehao00 <13663474696@163.com>
2019-06-23 13:34:50 +0800

    表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,18 +4,22 @@
           <div class="login-img">
           <img src="./logo_index.png" alt="黑马头条号">
           </div>
-      <el-form class="form-content" ref="form" :model="userform">
-        <el-form-item>
+      <el-form class="form-content" ref="form" :model="userform" :rules="rules">
+        <el-form-item prop="mobile">
           <el-input v-model="userform.mobile" placeholder="手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
             <!-- el-col 栅格系统  一共24列  :span 用来指定占用的大小  :offset 偏移量-->
-          <el-col :span="15">
+          <el-col :span="15" class="input-t">
             <el-input v-model="userform.code" placeholder="验证码"></el-input>
           </el-col>
-          <el-col :offset="1" :span="8">
+          <el-col :offset="1" :span="8" class="input-t">
             <el-button @click="handleSendCode">获取验证码</el-button>
           </el-col>
+        </el-form-item>
+        <el-form-item prop="agree">
+          <el-checkbox class="agree-checkbox" v-model="userform.agree"></el-checkbox>
+          <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></span>
         </el-form-item>
         <el-form-item>
           <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
@@ -36,13 +40,37 @@ export default {
     return {
       userform: {
         mobile: '',
-        code: ''
+        code: '',
+        agree: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { pattern: /\d{11}/, message: '请输入有效的手机号码', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { pattern: /\d{6}/, message: '请输入有效的验证码', trigger: 'blur' }
+        ],
+        agree: [
+          { required: true, message: '请同意用户协议' },
+          { pattern: /true/, message: '请同意用户协议' }
+        ]
       }
     }
   },
 
   methods: {
     handleLogin () {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        // 表单验证通过，提交登录请求
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -113,9 +141,9 @@ export default {
         align-items: center;
         background-image: url(./login_bg.jpg);
         .login-form{
-            width:600px;
+            width:500px;
             background-color: #fff;
-            padding:20px;
+            padding:30px;
             .login-img{
                display: flex;
                 justify-content: center;
@@ -124,6 +152,11 @@ export default {
             }
             .btn-login{
                 width:100%;
+            }
+            .agree-text{
+              a{
+                text-decoration: none;
+              }
             }
         }
     }
