fbd564098822ff907c86c6ce48313d44575722cf
hehao00 <13663474696@163.com>
2019-06-29 17:58:10 +0800

    评论管理--加载评论列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,55 +1,65 @@
 <template>
-<div>
-  <el-table
-      class="article-list"
-      :data="articles"
-      style="width: 100%">
-      <el-table-column
-        label="标题"
-        width="330">
-      </el-table-column>
-      <el-table-column
-        prop="title"
-        label="评论状态"
-        width="330">
-      </el-table-column>
-      <el-table-column
-        label="总评论数"
-        width="330">
-      </el-table-column>
-       <el-table-column
-          prop="pubdate"
-          label="粉丝评论数"
-          width="330">
-        </el-table-column>
-      <el-table-column
-        label="操作">
-         <template>
-            <el-button size="mini" type="primary" plain>修改</el-button>
-            <el-button size="mini" type="danger" plain >关闭评论</el-button>
-          </template>
+ <el-card class="box-card">
+  <div slot="header">
+    <span>评论管理</span>
+  </div>
+   <el-table :data="articles">
+      <el-table-column label="标题" prop="title"></el-table-column>
+      <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+      <el-table-column label="评论粉丝数" prop="fans_comment_count"></el-table-column>
+      <el-table-column label="允许评论">
+        <template slot-scope="scope">
+          <el-switch
+            :disabled="scope.row.disabled"
+            v-model="scope.row.comment_status"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            @change="handleChangeStatus(scope.row)">
+          </el-switch>
+        </template>
       </el-table-column>
     </el-table>
-   <el-pagination
-   class="page"
-   background
-   layout="prev, pager, next"
-   :total="1000">
-</el-pagination>
-</div>
+     <el-pagination
+    class="page"
+    background
+    layout="prev, pager, next"
+    :page-size="pageSize"
+    :total="totalCount"
+    @current-change="handleCurrentChange">
+   </el-pagination>
+</el-card>
 </template>
-
 <script>
 export default {
   name: '',
 
   data () {
     return {
-      articles: []
+      articles: [],
+      pageSize: 10, // 每页大小
+      totalCount: 0, // 总数据量
+      page: 1 // 当前页面
     }
   },
-
+  created () {
+    this.loadArticles()
+  },
   methods: {
+    async loadArticles () {
+      try {
+        const data = await this.$http({
+          method: 'GET',
+          url: '/articles',
+          params: {
+            response_type: 'comment'
+          }
+        })
+        this.articles = data.results
+      } catch (err) {
+        console.log(err)
+        this.$message.error('获取失败')
+      }
+    }
   }
 }
 </script>
