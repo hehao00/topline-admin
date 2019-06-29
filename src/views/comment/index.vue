e76b7f07f6f0a75102b41425efb81e8d446400ba
hehao00 <13663474696@163.com>
2019-06-29 20:03:27 +0800

    评论管理--分页数据

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -19,6 +19,14 @@
         </template>
       </el-table-column>
     </el-table>
+     <el-pagination
+      class="page"
+      background
+      layout="prev, pager, next"
+      :page-size="pageSize"
+      :total="totalCount"
+      @current-change="handleCurrentChange">
+   </el-pagination>
 </el-card>
 </template>
 <script>
@@ -27,13 +35,22 @@ export default {
 
   data () {
     return {
-      articles: []
+      articles: [],
+      pageSize: 10, // 每页大小
+      totalCount: 0, // 总数据量
+      page: 1 // 当前页面
     }
   },
   created () {
     this.loadArticles()
   },
   methods: {
+    // 查询
+    handleFilter () {
+      // 点击查询按钮 根据表单中的数据查询文章列表
+      this.page = 1 // 查询从第一页开始加载数据
+      this.loadArticles()
+    },
     // 获取评论列表
     async loadArticles () {
       try {
@@ -41,7 +58,9 @@ export default {
           method: 'GET',
           url: '/articles',
           params: {
-            response_type: 'comment'
+            response_type: 'comment',
+            page: this.page, // 页面
+            per_page: this.pageSize // 每页大小
           }
         })
         // 手动造一个数据 disabled 用来控制每一行的switch 开关的启用状态
@@ -49,10 +68,18 @@ export default {
           item.disabled = false
         })
         this.articles = data.results
+        this.totalCount = data.total_count
       } catch (err) {
         console.log(err)
         this.$message.error('获取失败')
       }
+    },
+    // 分页
+    handleCurrentChange (page) {
+      // 将数据中的页面码修改为当前最新改变的数据页面
+      this.page = page
+      // 页面改变 重新加载当前文章列表
+      this.loadArticles()
     },
     // 控制评论状态
     async handleChangeStatus (item) {
